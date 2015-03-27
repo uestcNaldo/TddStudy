462cb5518a609bc863a97eca4bd0e69cd47c0c1f
lookerson <361282125@qq.com>
2015-03-27 21:04:05 +0800

    modify some annotation in main_test.js

--- test/main_test.js
+++ test/main_test.js
@@ -6,7 +6,10 @@ var assert = require("assert");
 var War = require("../main/War");
 
 describe('War', function(){
-    it ("Test War.fight('张三', 20, 10, '李四', 20, 9) to simulation of the two attackers",function(){
+    it ("Test War.fight(FirstPeopleName, FirstPeopleHp, FirstPeopleAttack, SecondPeopleName, SecondPeopleHp, SecondPeopleAttack) function." +
+        "This function is simulation of the two attackers fight process." +
+        "In this test, we use War.fight('张三', 20, 10, '李四', 20, 9).",
+        function(){
         assert.equal(
             "张三攻击李四，李四受到10点伤害，剩余生命10\n" +
             "李四攻击张三，张三受到9点伤害，剩余生命11\n" +
@@ -15,7 +18,10 @@ describe('War', function(){
             War.fight('张三', 20, 10, '李四', 20, 9)
         );
     });
-    it ("Test War.fight('李s', 20, 9, '张san', 20, 10) to simulation of the two attackers",function(){
+    it ("Test War.fight(FirstPeopleName, FirstPeopleHp, FirstPeopleAttack, SecondPeopleName, SecondPeopleHp, SecondPeopleAttack) function." +
+        "This function is simulation of the two attackers fight process." +
+        "In this test, we use War.fight('李s', 20, 9, '张san', 20, 10)",
+        function(){
         assert.equal(
             "李s攻击张san，张san受到9点伤害，剩余生命11\n" +
             "张san攻击李s，李s受到10点伤害，剩余生命10\n" +
