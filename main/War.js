c6628623da92e4e1b8e284857a1ff425ce8c2a1c
lookerson <361282125@qq.com>
2015-03-27 09:53:39 +0800

    add question 2 answer

--- main/War.js
+++ main/War.js
@@ -4,15 +4,22 @@
 
 function fight(FirstPeopleName, FirstPeopleHp, FirstPeopleAttack, SecondPeopleName, SecondPeopleHp, SecondPeopleAttack){
     var bFirstPeopleWin = true;   // 若1赢，则为true，若2赢，则返回false
+    var FightProcess = "";        // 存储战斗过程和战斗结果
 
     while (1){
-        SecondPeopleHp -= FirstPeopleAttack;
+        var tempHpAndProcess;
+
+        tempHpAndProcess = AttackerFightDefenderOnce(FirstPeopleName, FirstPeopleAttack, SecondPeopleName, SecondPeopleHp);
+        SecondPeopleHp = tempHpAndProcess["DefenderHp"];
+        FightProcess += tempHpAndProcess["FightProcess"];
         if (SecondPeopleHp == 0){
             bFirstPeopleWin = true;
             break;
         }
 
-        FirstPeopleHp -= SecondPeopleAttack;
+        tempHpAndProcess = AttackerFightDefenderOnce(SecondPeopleName, SecondPeopleAttack, FirstPeopleName, FirstPeopleHp);
+        FirstPeopleHp = tempHpAndProcess["DefenderHp"];
+        FightProcess += tempHpAndProcess["FightProcess"];
         if (FirstPeopleHp == 0){
             bFirstPeopleWin = false;
             break;
@@ -21,11 +28,26 @@ function fight(FirstPeopleName, FirstPeopleHp, FirstPeopleAttack, SecondPeopleNa
 
     // 根据输赢返回结果
     if (bFirstPeopleWin){
-        return  FirstPeopleName + '胜利\n';
+        return FightProcess + FirstPeopleName + '胜利\n';
     }
     else{
-        return SecondPeopleName + '胜利\n';
+        return FightProcess + SecondPeopleName + '胜利\n';
     }
+}
+
+function AttackerFightDefenderOnce(AttackerName, AttackerAttack, DefenderName, DefenderHp){
+    // Denferder受到攻击，HP减少
+    DefenderHp -= AttackerAttack;
+    if (DefenderHp < 0){
+        DefenderHp = 0;
+    }
+
+    var process = AttackerName + "攻击" + DefenderName + "，" +
+                    DefenderName + "受到" + AttackerAttack + "点伤害，" +
+                    "剩余生命" + DefenderHp + "\n";
+
+    // 返回攻击后，Defender的HP和攻击过程
+    return {"DefenderHp" : DefenderHp, "FightProcess": process};
 }
 
 exports.fight = fight;
