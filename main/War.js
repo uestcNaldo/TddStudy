44844b8dcb46f452d47264cf673b5288490a2d2f
lookerson <lookerson.wm@gmail.com>
2015-03-30 21:56:05 +0800

    use mock_people to test

--- main/War.js
+++ main/War.js
@@ -3,23 +3,23 @@
  */
 
 var normal_people_class = {
-    create_new : function(name, hp, attack){
+    create_new : function(name, hp, aggerssivity){
         var normal_people = {};
         // 定义了人物的基本属性
         normal_people.name = name;
         normal_people.hp = hp;
-        normal_people.attack = attack;
+        normal_people.aggerssivity = aggerssivity;
 
-        normal_people.fight_once = function(defender){
+        normal_people.attack = function(defender){
             // 此函数模拟attacker向Defender攻击一次
-            defender.hp -= this.attack;
+            defender.hp -= this.aggerssivity;
             if (defender.hp < 0){
                 defender.hp = 0;
             }
 
             // 返回攻击过程
             return fight_process_once = this.name + "攻击" + defender.name + "，" +
-            defender.name + "受到" + this.attack + "点伤害，" +
+            defender.name + "受到" + this.aggerssivity + "点伤害，" +
             "剩余生命" + defender.hp + "\n";
 
         };
@@ -42,13 +42,13 @@ function fight(first_people, second_people){
     var fight_process = "";        // 存储战斗过程和战斗结果
 
     while (1){
-        fight_process += first_people.fight_once(second_people);
+        fight_process += first_people.attack(second_people);
         if (second_people.is_dead()){
             fight_process += (first_people.name + '胜利\n');
             break;
         }
 
-        fight_process += second_people.fight_once(first_people);
+        fight_process += second_people.attack(first_people);
         if (first_people.is_dead()){
             fight_process += (second_people.name + '胜利\n');
             break;
--- test/main_test.js
+++ test/main_test.js
@@ -5,24 +5,80 @@
 var assert = require("assert");
 var War = require("../main/War");
 
+function make_mock_person (name, hp, aggerssivity) {
+    var mock_person = new Object();
+    mock_person.name = name;
+    mock_person.hp = hp;
+    mock_person.aggerssivity = aggerssivity;
+    mock_person.count = 0;      // 记录第几次调用attack函数
+
+    return mock_person;
+}
+
 describe('War', function(){
-    it ("Test a function: " +
+    // 测试War.fight()函数
+    it (
+        "Test a function: " +
         "There is two people." +
         "张三 HP 20, attack 10." +
         "李四 HP 20, attack 9." +
         "They took turns to attack." +
         "This function output the process and result of fight.",
         function(){
-        first_people = War.normal_people_class.create_new('张三', 20, 10);
-        second_people = War.normal_people_class.create_new('李四', 20, 9);
-        assert.equal(
-            "张三攻击李四，李四受到10点伤害，剩余生命10\n" +
-            "李四攻击张三，张三受到9点伤害，剩余生命11\n" +
-            "张三攻击李四，李四受到10点伤害，剩余生命0\n" +
-            "张三胜利\n",
-            War.fight(first_people, second_people)
-        );
-    });
+            // 模拟first_people
+            var first_people = make_mock_person("张三", 20, 10);
+            first_people.attack = function () {
+                first_people.count ++;
+                switch (first_people.count){
+                    case 1:{
+                        return "张三攻击李四，李四受到10点伤害，剩余生命10\n";
+                    }
+                    case 2:{
+                        return "张三攻击李四，李四受到10点伤害，剩余生命0\n";
+                    }
+                    default :{
+                        return;
+                    }
+                }
+            }
+            first_people.is_dead = function () {
+                return false;
+            }
+
+
+            // 模拟second_people
+            var second_people = make_mock_person("李四", 20, 9);
+            second_people.attack = function(){
+                second_people.count ++;
+                switch (second_people.count){
+                    case 1:{
+                        return "李四攻击张三，张三受到9点伤害，剩余生命11\n"
+                    }
+                    default:{
+                        return;
+                    }
+                }
+            }
+            second_people.is_dead = function () {
+                if (second_people.count == 1){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+
+            // 测试
+           assert.equal(
+                "张三攻击李四，李四受到10点伤害，剩余生命10\n" +
+                "李四攻击张三，张三受到9点伤害，剩余生命11\n" +
+                "张三攻击李四，李四受到10点伤害，剩余生命0\n" +
+                "张三胜利\n",
+                War.fight(first_people, second_people)
+            );
+        }
+    );
+
     it (
         "Test a function: " +
         "There is two people." +
@@ -31,19 +87,64 @@ describe('War', function(){
         "They took turns to attack." +
         "This function output the process and result of fight.",
         function(){
-            first_people = War.normal_people_class.create_new('李s', 20, 9);
-            second_people = War.normal_people_class.create_new('张san', 20, 10);
-        assert.equal(
-            "李s攻击张san，张san受到9点伤害，剩余生命11\n" +
-            "张san攻击李s，李s受到10点伤害，剩余生命10\n" +
-            "李s攻击张san，张san受到9点伤害，剩余生命2\n" +
-            "张san攻击李s，李s受到10点伤害，剩余生命0\n" +
-            "张san胜利\n",
-            War.fight(first_people, second_people)
-        );
+            // 模拟first_people
+            var first_people = make_mock_person("李s", 20, 9);
+            first_people.attack = function () {
+                first_people.count ++;
+                switch (first_people.count){
+                    case 1:{
+                        return "李s攻击张san，张san受到9点伤害，剩余生命11\n";
+                    }
+                    case 2:{
+                        return "李s攻击张san，张san受到9点伤害，剩余生命2\n";
+                    }
+                    default:{
+                        return;
+                    }
+                }
+            }
+            first_people.is_dead = function () {
+                if (first_people.count == 2){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            // 模拟second_people
+            var second_people = make_mock_person("张san", 20, 10);
+            second_people.attack = function () {
+                second_people.count ++;
+                switch (second_people.count) {
+                    case 1:{
+                        return "张san攻击李s，李s受到10点伤害，剩余生命10\n";
+                    }
+                    case 2:{
+                        return "张san攻击李s，李s受到10点伤害，剩余生命0\n";
+                    }
+                    default :{
+                        return;
+                    }
+                }
+            }
+            second_people.is_dead = function () {
+                return false;
+            }
+
+            // 测试
+            assert.equal(
+                "李s攻击张san，张san受到9点伤害，剩余生命11\n" +
+                "张san攻击李s，李s受到10点伤害，剩余生命10\n" +
+                "李s攻击张san，张san受到9点伤害，剩余生命2\n" +
+                "张san攻击李s，李s受到10点伤害，剩余生命0\n" +
+                "张san胜利\n",
+                War.fight(first_people, second_people)
+            );
         }
     );
 
+    // 测试War.normal_people_class.attack()函数
     it (
         "Test a function: " +
         "There is two people." +
@@ -56,8 +157,8 @@ describe('War', function(){
             second_people = War.normal_people_class.create_new('LiSi', 11, 20);
             assert.equal(
                 "ZhangSan攻击LiSi，LiSi受到10点伤害，剩余生命1\n",
-                first_people.fight_once(second_people)
+                first_people.attack(second_people)
             );
         }
-    )
+    );
 })
