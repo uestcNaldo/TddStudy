80828d6c2fc30e58efba212e56b48160697d9320
lookerson <lookerson.wm@gmail.com>
2015-04-26 15:18:31 +0800

    eliminate duplicate code

--- main/War.js
+++ main/War.js
@@ -2,22 +2,40 @@
  * Created by lookerson on 2015/3/24.
  */
 
+function fight_people_once(attacker, defender) {
+    var fight_process = attacker.attack(defender);
+
+    var is_win = false;
+    if (defender.is_dead()) {
+        fight_process += ( attacker.get_name() + '胜利\n');
+        is_win = true;
+    }
+
+    return {
+        "process" :fight_process,
+        "result" :is_win
+    }
+}
+
 function fight(first_people, second_people){
     var fight_process = "";        // 存储战斗过程和战斗结果
+    var one_people_win = false;
 
-    while (1){
-        fight_process += first_people.attack(second_people);
-        if (second_people.is_dead()){
-            fight_process += (first_people.get_name() + '胜利\n');
-            break;
-        }
+    var attacker = first_people;
+    var defender = second_people;
 
-        fight_process += second_people.attack(first_people);
-        if (first_people.is_dead()){
-            fight_process += (second_people.get_name() + '胜利\n');
-            break;
-        }
-    }
+    do{
+        // 攻击者向防御者攻击
+        var process_and_result  = fight_people_once(attacker, defender);
+        fight_process += process_and_result['process'];
+        one_people_win = process_and_result['result'];
+
+        // 交换攻击者和防御者
+        var temp_people = attacker;
+        attacker = defender;
+        defender = temp_people;
+
+    }while (!one_people_win);
 
     return fight_process;
 }
